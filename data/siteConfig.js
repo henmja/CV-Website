8ba18d242b85f9f59490daa2b159306328e45e56
Henrik Mjaaland <silje.torland91@gmail.com>
2019-07-26 13:12:06 +0200

    Update siteConfig.js

--- data/siteConfig.js
+++ data/siteConfig.js
@@ -6,7 +6,7 @@ module.exports = {
   githubUsername: 'henmja',
   authorAvatar: '/images/avatar.jpeg',
   authorDescription: `I am a computer engineer from Sandnes, Norway. I have a bachelor degree in Computer Science (from the University of Stavanger), and I am currently working on a master's degree in Computer Science (due spring '20). <br /><br/> Computers has always been a hobby for me. Even though I am studying Computer Science, I am planning to major in Data Science, because I'm fascinated by how it can optimize desicion making and implement AI.<br /><br /> At the University of Stavanger we learned programming languages such as Java, Python, SQL, JS and MATLAB. <br /><br/>
-    On my sparetime I went deeper into web programming and learned to use different frameworks such as Node, React, Angular and Gatsby (which I used for this website) <br /><br />
+    On my sparetime I went deeper into web programming and learned to use different frameworks such as Node, React, Angular and Gatsby (which I used for this website). <br /><br />
     </strong>`,
   skills: [
     {
@@ -64,6 +64,10 @@ module.exports = {
     {
       name: 'PHP',
       level: 50
+    },
+    {
+      name: 'latex',
+      level: 80
     },
   ],
   jobs: [
